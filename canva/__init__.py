cf643b080ba756e532b7d2083e13f698296090f1
yxm <yxm.dev@gmx.ie>
2025-03-25 12:06:21 -0300

    ...

--- canva/__init__.py
+++ canva/__init__.py
@@ -6,24 +6,3 @@ from dotenv import load_dotenv
 
 canva = Canva
 cv    = canva
-
-MAIN_DIR = Path(os.path.dirname(__file__), '..').resolve()
-token_file = os.path.join(MAIN_DIR, 'canva.json')
-load_dotenv(os.path.join(MAIN_DIR, '.env'))
-
-client_id = os.getenv('CANVA_CLIENT_ID')
-client_secret = os.getenv('CANVA_CLIENT_SECRET')
-
-canva.init(client_id, client_secret, token_file)
-
-design_id = cv.design.get.id('vórtice', client_id, client_secret, token_file)
-export_id = cv.export.design.png(design_id, True, client_id, client_secret, token_file)
-
-while True:
-    export_status = cv.export.get.status(export_id, client_id, client_secret, token_file)
-    if export_status == 'success':
-        url = cv.export.get.url(export_id, client_id, client_secret, token_file)
-        break
-
-print(url)
-
